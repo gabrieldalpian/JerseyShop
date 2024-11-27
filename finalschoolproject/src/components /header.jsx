b23ce3e67d1ad3f4106ab94ee9b75975dcce4006
Dalpian77 <gabrieldalpianpedro@gmail.com>
2024-11-27 02:00:55 -0500

    update

---
+++ finalschoolproject/src/components /header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/programs">Programs</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
