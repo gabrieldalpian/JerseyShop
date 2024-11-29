a28d81c18fabcca36519d09b9b6144fc8fc1709b
Dalpian77 <gabrieldalpianpedro@gmail.com>
2024-11-28 23:45:45 -0500

    update

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './app.css';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>
+        <p>Body content goes here</p>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+      <header>
+        <nav>
+          <ul>
+            <li>Shop</li>
+            <li>Cart</li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header;
