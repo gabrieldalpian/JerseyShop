0d4c9a42091fb49783dd7c045cf1df307af0a061
Dalpian77 <gabrieldalpianpedro@gmail.com>
2024-11-29 17:20:29 -0500

    best selllers section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './app.css';
+import image1 from './assets/jzy.jpg';
+import image2 from './assets/intermilano.jpg';
+import image3 from './assets/newcastle.jpg';
+import image4 from './assets/arsenal.jpg';
+import image5 from './assets/vasco.jpg';
 
 
 function App() {
@@ -10,8 +15,42 @@ function App() {
       <Header />
 
       <div className="body-content">
-        <p>Body content goes here</p>
+        <img src={image1} />
       </div>
+
+      <div className='featured-products'>
+        Best Sellers 
+      </div>
+
+      <div className='jerseys-images'>
+
+        <div className="jersey-item">
+          <img src={image2} alt="Inter Milan jersey" />
+          <p>Inter Milan jersey<br></br> $60</p>
+         
+        </div>
+        <div className="jersey-item">
+          <img src={image3} alt="Newcastle jersey" />
+          <p>Newcastle jersey<br></br> $60</p>
+          
+
+        </div>
+        <div className="jersey-item">
+          <img src={image4} alt="Arsenal jersey" />
+          <p>Arsenal jersey<br></br> $60</p>
+          
+
+        </div>
+        <div className="jersey-item">
+          <img src={image5} alt="Vasco da Gama jersey" />
+          <p>Vasco da Gama jersey<br></br> $60</p>
+        </div>
+      </div>       
+
+      <div className="button-view">
+        <button> SHOP ALL </button>
+      </div>
+
 
       <Footer />
     </>
