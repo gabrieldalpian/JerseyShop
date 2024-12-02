e2b45a0341a84cb95355d7c8a7dcdf5501a10405
Dalpian77 <gabrieldalpianpedro@gmail.com>
2024-12-02 12:56:05 -0500

    items section done

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -6,6 +6,38 @@ import image9 from '../assets/arsenalthree.jpg';
 import image10 from '../assets/fulham.jpg';
 import image11 from '../assets/mancity.jpg';
 import image12 from '../assets/manutd.jpg';
+import image13 from '../assets/internacional.jpg';
+import image14 from '../assets/corinthians.jpg';
+import image15 from '../assets/gremio.jpg';
+import image16 from '../assets/flamengo.jpg';
+import image17 from '../assets/fluminense.jpg';
+import image18 from '../assets/bayerntwo.jpg';
+import image19 from '../assets/bourssia.jpg';
+import image20 from '../assets/frankfurt.jpg';
+import image21 from '../assets/leverkusen.jpg';
+import image22 from '../assets/schalke.jpg';
+import image23 from '../assets/internazionale.jpg';
+import image24 from '../assets/juventus.jpg';
+import image25 from '../assets/internazionaletwo.jpg';
+import image26 from '../assets/lazio.jpg';
+import image27 from '../assets/milan.jpg';
+import image28 from '../assets/barcelona.jpg';
+import image29 from '../assets/madrid.jpg';
+import image30 from '../assets/lacoruna.jpg';
+import image31 from '../assets/rayo.jpg';
+import image32 from '../assets/nsei.jpg';
+import image33 from '../assets/intermiami.jpg';
+import image34 from '../assets/lafc.jpg';
+import image35 from '../assets/philadelphia.jpg';
+import image36 from '../assets/lagalaxy.jpg';
+import image37 from '../assets/orlando.jpg';
+import image38 from '../assets/paris.jpg';
+import image39 from '../assets/marselia.jpg';
+import image40 from '../assets/lyon.jpg';
+import image41 from '../assets/marseliatwo.jpg';
+import image42 from '../assets/paristwo.jpg';
+
+
 
 
 function ShopPage() {
@@ -48,6 +80,202 @@ function ShopPage() {
             </div>
             <div className="shop-page-jersey-item">
                 <img src={image12} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image13} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image14} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image15} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image16} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image17} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image18} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image19} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image20} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image22} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image21} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image29} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image28} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image30} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image31} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image32} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image33} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image34} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image35} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image37} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image36} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div>
+            <div className="shop-page-jersey-item">
+                <img src={image38} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div><div className="shop-page-jersey-item">
+                <img src={image39} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div><div className="shop-page-jersey-item">
+                <img src={image41} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div><div className="shop-page-jersey-item">
+                <img src={image40} alt="Manchester United Jersey" />
+                <p>
+                    Manchester United Jersey 
+                    <br />
+                    $60
+                </p>
+            </div><div className="shop-page-jersey-item">
+                <img src={image42} alt="Manchester United Jersey" />
                 <p>
                     Manchester United Jersey 
                     <br />
