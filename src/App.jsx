bc6d40308b46d2270b45db48d32c1fdb3043769b
Dalpian77 <gabrieldalpianpedro@gmail.com>
2025-02-14 22:08:45 -0500

    logic and responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -95,40 +95,32 @@ function App() {
               <div className="button-view">
                 <Link to="/shop" className="button">Shop All</Link>
               </div>
-              <section className="about-container">
-  <div className="about-title">
-    <h2>
-      About our company
-    </h2>
-  </div>
-  <div className="about-text">
-    <p>
-      Welcome to Jersey Paradise! We're passionate about delivering high-quality,
-      authentic jerseys that let you proudly showcase your team spirit in style.
-      Whether you're a die-hard fan or just love the game, we have the perfect jersey
-      waiting for you!
-    </p>
-  </div>
-</section>
-              
 
-              <section>
+              <section className="about-section">
                 <div className="imagem">
                   <img src={image7} alt="About Us" />
                 </div>
+                <div className="about-container">
+                  <div className="about-title">
+                    <h2>About our company</h2>
+                </div>
+                <div className="about-text">
+                  <p>Welcome to Jersey Paradise! We're passionate about delivering high-quality, authentic jerseys that let you proudly showcase your team spirit in style. Whether you're a die-hard fan or just love the game, we have the perfect jersey waiting for you!</p>
+                </div>
+                </div>
               </section>
+
               <div className='main-footer'>
                 <Footer />
               </div>
               <br></br>
               <br></br>
               <br></br>
-
             </>
           }
         />
         <Route path="/shop" element={<ShopPage addToCart={addToCart} successMessage={successMessage} />} />
-        <Route path="/cart" element={<CartPage cart={cart} />} />
+        <Route path="/cart" element={<CartPage cart={cart} successMessage={successMessage} />} />
         <Route path="/App" element={<App/>} />
       </Routes>
     </>
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Footer from '../components/Footer';
 
-
 function CartPage({ cart }) {
 
     const calculateTotal = () => {
